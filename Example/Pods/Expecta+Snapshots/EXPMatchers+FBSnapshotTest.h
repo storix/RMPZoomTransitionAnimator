5b355e31d297328d9e8100f616ab685348201cbc
Stan Mots <stuffokator@gmail.com>
2017-01-23 23:28:01 +0200

    Update pods

--- Example/Pods/Expecta+Snapshots/EXPMatchers+FBSnapshotTest.h
+++ Example/Pods/Expecta+Snapshots/EXPMatchers+FBSnapshotTest.h
@@ -1,12 +1,5 @@
-//
-//  EXPMatchers+FBSnapshotTest.h
-//  Artsy
-//
-//  Created by Daniel Doubrovkine on 1/14/14.
-//  Copyright (c) 2014 Artsy Inc. All rights reserved.
-//
-
 #import <Expecta/Expecta.h>
+#import "ExpectaObject+FBSnapshotTest.h"
 
 @interface EXPExpectFBSnapshotTest : NSObject
 @end
